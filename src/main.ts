8cc72d0d3937b757b2ede1a597e896a4a1c16e27
nomnng <denis.rbn0@gmail.com>
2026-06-09 00:48:55 +0300

    Updated prettier configuration

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	const config = new DocumentBuilder()
-		.setTitle('API docs')
-		.setVersion('1.0')
+		.setTitle("API docs")
+		.setVersion("1.0")
 		.build();
 	const documentFactory = () => SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup('api', app, documentFactory);
+	SwaggerModule.setup("api", app, documentFactory);
 
 	await app.listen(process.env.PORT ?? 3000);
 }
